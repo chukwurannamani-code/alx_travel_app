0e7ed8abfaab694e9109a51436aa868fc3973041
chukwurannamani-code <chukwura.nnamani@simplexi.io>
2025-11-09 21:55:21 +0100

    wip

---
+++ listings/views.py
@@ -0,0 +1,8 @@
+from rest_framework import generics
+from .models import Property
+from .serializers import PropertySerializer
+
+class PropertyList(generics.ListCreateAPIView):
+    queryset = Property.objects.all()
+    serializer_class = PropertySerializer
+
